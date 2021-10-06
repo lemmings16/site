7b0fcdccd2cf224fd294913f2a5706285a201869
Anouar Ameziane <58337960+lemmings16@users.noreply.github.com>
2021-10-06 08:30:11 +0000

    dictionary works and form

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 
 
 
+from flask import Flask, render_template, request
 
-
-from flask import Flask, render_template
 
 # Create a flask app
 app = Flask(
@@ -12,14 +11,58 @@ app = Flask(
   static_folder='static'
 )
 
+
+games = [
+  {
+    'naam': 'Valorant', 
+    'verkochte_kopies': '14 miljoen',
+    'spelers_per_dag': '251 duizend', 
+    'leeftijden': '1 jaar en 2 maanden', 
+    'genres': 'First-Person Shooter(FPS)'
+  },
+  {
+    'naam': 'Minecraft',
+    'verkochte_kopies': '200 miljoen',
+    'spelers_per_dag': '857 duizend',
+    'leeftijden': '10 jaar', 
+    'genres': 'Sandbox'
+  },
+  {
+    'naam': 'GTAV',
+    'verkochte_kopies': '120 miljoen',
+    'spelers_per_dag': '130 duizend',
+    'leeftijden': '8 jaar',
+    'genres': 'Open World',
+  },
+   {
+    'naam': 'CS:GO',
+    'verkochte_kopies': '30 miljoen', 
+    'spelers_per_dag': '507 duizend',
+    'leeftijden': '9 jaar',
+    'genres': 'First-Person Shooter'
+  }
+
+]
+
 # Index page (now using the index.html file)
 @app.route('/')
-def index():
-  return render_template('index.html')
+def hello():
+  return render_template('index.html', games=games)
 
-@app.route("/info/<string:game>/")
-def hello(game):
-    return render_template('shop.html', game=game)
+
+
+ 
+  
+
+
+
+@app.route('/game_invoer', methods=['POST'])
+def invoer_game():
+  invoer_game = request.form['invoer_game']
+  return render_template('shop.html', 
+                         invoer_game = invoer_game)
+
+
 
 if __name__ == '__main__':
   # Run the Flask app
